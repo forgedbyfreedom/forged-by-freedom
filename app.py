23c7b92c09fcb7aa4ad28561bbe619a2b9333ac4
Bran Antonelli <forgedbyfreedom@gmail.com>
2025-12-23 15:54:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,11 @@ from typing import List, Optional
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+
+CORS(
+    app,
+    resources={r"/*": {"origins": "*"}},
+    supports_credentials=False
 
 
 from pinecone import Pinecone
